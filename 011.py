2dabf06b3d628c8a474394667d05bafdc3726bae
Daniele <danielejsantosxavier@gmail.com>
2023-03-29 20:12:07 -0300

    Create 011.py

---
+++ 011.py
@@ -0,0 +1,7 @@
+#delta == B² -4 *a *c
+A = int(input('Valor de A: '))
+B = int(input('Valor de B: '))
+C = int(input('Valor de C: '))
+#delta = B**2-4*A*C
+delta = B*B-4*A*C
+print ('O valor do delta é {}' .format(delta))
