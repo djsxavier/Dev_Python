6328b991c891498d5698b7451721d04e71d04184
Daniele <danielejsantosxavier@gmail.com>
2023-03-29 19:33:44 -0300

    Create 010.py

---
+++ 010.py
@@ -0,0 +1,5 @@
+largura = float(input("Informe a largura: "))
+altura = float(input("Informe a altura: "))
+calculo = largura * altura
+tinta = calculo / 2
+print ("Para pintar a parede, você precisa de {} litros de tinta" .format (tinta))
