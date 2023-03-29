89ee84a0c59c238250a0ef48f2b358525600c214
Daniele <danielejsantosxavier@gmail.com>
2023-03-29 20:12:21 -0300

    Create 014.py

---
+++ 014.py
@@ -0,0 +1,4 @@
+Km = float(input('Quantidade de KM percorrido: '))
+Dias = int(input('Quantidade de dias: '))
+Calculo = (Dias*90)+(Km*0.20)
+print ("Preço total a pagar R$ {:.2f}" .format(Calculo))
