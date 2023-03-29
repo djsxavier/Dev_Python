7d55d080719ebcee5db0ced06f00c059d959c878
Daniele <danielejsantosxavier@gmail.com>
2023-03-29 20:12:16 -0300

    Create 013.py

---
+++ 013.py
@@ -0,0 +1,3 @@
+salario = float (input('Digite o salário do funcionário: '))
+aumento = salario +(salario *15 / 100)
+print ('O aumento de salário do funcionário será de R$ {:.2f}' .format(aumento))
