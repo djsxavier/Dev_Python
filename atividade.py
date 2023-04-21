ab967dfdb1b0025a7850860a3179bd2fa0d58d68
Daniele <danielejsantosxavier@gmail.com>
2023-04-21 20:48:10 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,8 @@
+#idade da pessoa, acima de 30 anos velha e se não ela está nova
+idade = int(input('Digite sua idade: '))
+if idade > 30:
+    print('Você está velha')
+    print('Seus dias estão contados')
+else:
+    print('Você está nova')
+    print('Você ainda vai viver muito')
